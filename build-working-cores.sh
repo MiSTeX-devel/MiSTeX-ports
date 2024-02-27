91cbabec01e23c8d736ca11b04bec7a3415ed620
Hans Baier <foss@hans-baier.de>
2024-02-28 05:29:39 +0700

    parallelize build-working-cores

--- build-working-cores.sh
+++ build-working-cores.sh
@@ -4,8 +4,6 @@ WORKING_CORES=$(awk '{ print $1 " " $5}' cores/Readme.md | grep works | awk '{ p
 BOARD="$1"
 
 [ -z "$BOARD" ] && BOARD=mistex_boards/qmtech_xc7a100t_mistex.py
+[ -z "$NUM_PROCS" ] && NUM_PROCS=1
 
-for core in $WORKING_CORES
-do
-    time python3 $BOARD cores/$core
-done
+echo $WORKING_CORES | xargs -d ' ' -I @ -P $NUM_PROCS python3 $BOARD cores/@
