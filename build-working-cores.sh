0313bb2bdb00ff56cdbca0f1d1f93792ac8119de
Hans Baier <foss@hans-baier.de>
2024-02-27 15:02:55 +0700

    fix bug in build-working-cores.sh

--- build-working-cores.sh
+++ build-working-cores.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 WORKING_CORES=$(awk '{ print $1 " " $5}' cores/Readme.md | grep works | awk '{ print $1 }' | sed 's/|//g')
 
+BOARD="$1"
+
+[ -z "$BOARD" ] && BOARD=mistex_boards/qmtech_xc7a100t_mistex.py
+
 for core in $WORKING_CORES
 do
-    time python $1 cores/$core
+    time python3 $BOARD cores/$core
 done
