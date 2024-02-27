579d4c0cb35fa711e23d58fa3a0d7df4bf1f6cb3
Hans Baier <foss@hans-baier.de>
2024-02-27 15:20:36 +0700

    add dependency to setup-aws.sh

--- setup-aws.sh
+++ setup-aws.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # this sets up an AWS Vivado instance to compile MiSTeX ports
 sudo apt update
-sudo apt install -y python3-pip
-sudo apt install -y python3-venv
+sudo apt install -y python3-pip python3-venv meson gcc-riscv64-unknown-elf
 git clone --recursive https://github.com/MiSTeX-devel/MiSTeX-ports.git ports
 cd ports
 python -m venv venv
