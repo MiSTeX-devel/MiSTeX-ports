a2c5f6651772410dd610c1b91ed08ba9f184642d
Hans Baier <foss@hans-baier.de>
2024-02-27 14:42:34 +0700

    add build-working-cores.sh

---
+++ build-working-cores.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+WORKING_CORES=$(awk '{ print $1 " " $5}' cores/Readme.md | grep works | awk '{ print $1 }' | sed 's/|//g')
+
+for $core in $WORKING_CORES
+do
+    time python $1 $core
+done
