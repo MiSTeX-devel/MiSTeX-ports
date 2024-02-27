7745807159441ba807e9bb3f49ba11f7d6b278d3
Hans Baier <foss@hans-baier.de>
2024-02-27 14:46:00 +0700

    add setup-aws.sh

---
+++ setup-aws.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this sets up an AWS Vivado instance to compile MiSTeX ports
+sudo apt update
+sudo apt install -y python3-pip
+git clone https://github.com/MiSTeX-devel/MiSTeX-ports.git ports
+cd ports
+python -m venv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
