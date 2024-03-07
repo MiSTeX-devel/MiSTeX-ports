c2f0eea0176fd1febd374b7826dd8ee835a80c01
Hans Baier <foss@hans-baier.de>
2024-03-08 04:52:52 +0700

    install-cores.sh: add support for different FPGAs

--- install-cores.sh
+++ install-cores.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 DIR="$1"
+SUFFIX="$2"
 
 [ -z "$DIR" ] && DIR=build/qmtech_xc7a100t_mistex
+[ -z "$SUFFIX" ] && SUFFIX=_A100T
 
 for i in $(find $DIR -name \*.bit)
 do
   BASE=$(basename $i)
-  DEST=${BASE/_MiSTeX/}
+  DEST=${BASE/_MiSTeX/${SUFFIX}}
   if [[ "$i" == *Arcade_* ]] then
     DEST=${BASE/Arcade_/Arcade-}
     DEST=${DEST/_MiSTeX/}
-    scp $i root@orangepizero2w.lan:/media/fat/_Arcade/cores/$DEST
+    scp $i root@orangepizero2w.lan:/media/fat/_Arcade${SUFFIX}/cores/$DEST
   elif  [[ "$i" == */Menu_MiSTeX.bit ]] then
-    scp $i root@orangepizero2w.lan:/media/fat/menu.bit
+    scp $i root@orangepizero2w.lan:/media/fat/menu${SUFFIX}.bit
   elif [[ "$i" == *SMS_MiST* ]] || [[ "$i" == *NeoGeo_MiST* ]] then
     scp $i root@orangepizero2w.lan:/media/fat/_Console/$DEST
   else
