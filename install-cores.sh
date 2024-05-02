fd2ee5c40ad972cbf82433dd133bb932541462a8
Hans Baier <foss@hans-baier.de>
2024-05-02 10:46:57 +0700

    install-cores.sh: add jotego core support

--- install-cores.sh
+++ install-cores.sh
@@ -5,20 +5,25 @@ SUFFIX="$2"
 
 [ -z "$DIR" ] && DIR=build/qmtech_xc7a100t_mistex
 [ -z "$SUFFIX" ] && SUFFIX=_A100T
+[ -z "$HOST" ] && HOST=orangepizero2w.local
 
 for i in $(find $DIR -name \*.bit)
 do
   BASE=$(basename $i)
   DEST=${BASE/_MiSTeX/${SUFFIX}}
-  if [[ "$i" == *Arcade_* ]] then
+  TCL=$(dirname $i)/$(basename $i .bit).tcl
+  if grep -q jtcores $TCL; then
+    DEST=${BASE/_MiSTeX/}
+    scp $i root@${HOST}:/media/fat/_Arcade${SUFFIX}/cores/jt${DEST}
+  elif [[ "$i" == *Arcade_* ]] then
     DEST=${BASE/Arcade_/Arcade-}
     DEST=${DEST/_MiSTeX/}
-    scp $i root@orangepizero2w.lan:/media/fat/_Arcade${SUFFIX}/cores/$DEST
+    scp $i root@${HOST}:/media/fat/_Arcade${SUFFIX}/cores/$DEST
   elif  [[ "$i" == */Menu_MiSTeX.bit ]] then
-    scp $i root@orangepizero2w.lan:/media/fat/menu${SUFFIX}.bit
+    scp $i root@${HOST}:/media/fat/menu${SUFFIX}.bit
   elif [[ "$i" == *SMS_MiST* ]] || [[ "$i" == *NES_MiST* ]] ||  [[ "$i" == *NeoGeo_MiST* ]] then
-    scp $i root@orangepizero2w.lan:/media/fat/_Console/$DEST
+    scp $i root@${HOST}:/media/fat/_Console/$DEST
   else
-    scp $i root@orangepizero2w.lan:/media/fat/_Utility/$DEST
+    scp $i root@${HOST}:/media/fat/_Utility/$DEST
   fi
 done
