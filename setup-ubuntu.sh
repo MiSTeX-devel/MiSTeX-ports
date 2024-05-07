1bbb1b12ed74cb8a1ddf5103b89192b969774e9e
Hans Baier <foss@hans-baier.de>
2024-05-08 05:28:41 +0700

    remove broken submodule commands from setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,9 +4,6 @@ sudo apt update
 sudo apt install -y python3-pip python3-venv meson gcc-riscv64-unknown-elf
 git clone --recursive https://github.com/MiSTeX-devel/MiSTeX-ports.git ports
 cd ports
-cd cores/Arcade-jtcores/jtcores/
-git -c submodule."modules/jt900h".update=none -c submodule."modules/jtframe/target/pocket".update=none submodule update --init --recursive
-cd -
 pip3 install virtualenv
 python3 -m venv venv
 source ./venv/bin/activate
