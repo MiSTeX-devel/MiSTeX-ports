e68d369ace41d1b1145eded27b7e5daf185cb8dd
Hans Baier <foss@hans-baier.de>
2024-05-07 14:11:16 +0700

    fix setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -7,7 +7,7 @@ cd ports
 cd cores/Arcade-jtcores/jtcores/
 git -c submodule."modules/jt900h".update=none -c submodule."modules/jtframe/target/pocket".update=none submodule update --init --recursive
 cd -
-pip install virtualenv
+pip3 install virtualenv
 python3 -m venv venv
 source ./venv/bin/activate
 pip3 install -r requirements.txt
